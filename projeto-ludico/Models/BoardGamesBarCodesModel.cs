b3902e83adcc66aab4da55e69ab6ccd7d7b5fab7
Jackoki <kenji.inoue@hotmail.com>
2025-05-28 21:04:57 -0300

    Insereção de novas classes models

---
+++ projeto-ludico/Models/BoardGamesBarCodesModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_ludico.Models
+{
+    internal class BoardGamesBarCodesModel
+    {        
+        // Propriedades
+        public int id { get; set; }
+        public string bar_code { get; set; }
+        public int id_board_game { get; set; }
+
+        public BoardGamesBarCodesModel() { }
+    }
+}
---
+++ projeto-ludico/Models/BoardGamesNamesModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_ludico.Models
+{
+    internal class BoardGamesNamesModel
+    {        
+        // Propriedades
+        public int id { get; set; }
+        public string name { get; set; }
+        public bool is_principal { get; set; }
+        public int id_board_game { get; set; }
+
+        public BoardGamesNamesModel() { }
+    }
+}
