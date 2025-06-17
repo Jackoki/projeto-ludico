4acdcf302eb012d6a154a7e420a1486eca6daf2d
Pedro-Landgraf <143462985+Pedro-Landgraf@users.noreply.github.com>
2025-06-17 12:59:35 -0300

    Update RPGDelete.cs

--- projeto-ludico/View/RPGForms/RPGDelete.cs
+++ projeto-ludico/View/RPGForms/RPGDelete.cs
@@ -20,11 +20,13 @@ namespace projeto_ludico.View.RPGForms
 
         public void DeleteRPG(DataGridViewRow row)
         {
+            //Análisa se o id é diferente de nulo e se consegue converter para INT
             if (row.Cells["id"].Value != null && int.TryParse(row.Cells["id"].Value.ToString(), out int id))
             {
                 RPGModel rpgModel = new RPGModel();
                 rpgModel.id = id;
 
+                // Exibe uma mensagem de confirmação, se o usuário apertar Sim, irá deletar pelo controller
                 DialogResult result = MessageBox.Show("Tem certeza de que deseja excluir este RPG?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 
                 if (result == DialogResult.Yes)
