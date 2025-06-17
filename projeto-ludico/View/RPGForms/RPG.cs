b739c3caf27769dd856e4c45bb9558b3e3b4b4f4
Pedro-Landgraf <143462985+Pedro-Landgraf@users.noreply.github.com>
2025-06-17 12:56:43 -0300

    Update RPG.cs

--- projeto-ludico/View/RPGForms/RPG.cs
+++ projeto-ludico/View/RPGForms/RPG.cs
@@ -13,6 +13,7 @@ namespace projeto_ludico.View.RPGForms
             dataViewer.CellContentClick += DataViewer_CellContentClick;
         }
 
+         //Realiza a chamada das informações do SQL, passamos as colunas que queremos e as renomeamos no da DataGrid
         private void ConfigureRPGViewer()
         {
             string[] desiredColumns = { "name", "id" };
@@ -21,24 +22,32 @@ namespace projeto_ludico.View.RPGForms
                 { "name", "Nome" },
             };
 
+            //Passamos o null no final pois não temos nenhum JOIN a ser retornado na tabela
+            //Passamos o true para dizer não iremos adicionar o botão "Gerenciar" na linha
             ConfigureDataViewer(dataViewer, "role_play_games", desiredColumns, columnMappings, null, true);
             OccultColumns(dataViewer, "id");
         }
 
+        //Função responsável para realizar a atualização de pesquisa quando o usuário realizar a pesquisa no formulário
         private void PerformSearch(string searchString)
         {
+            // Passamos o nome da coluna que queremos que seja retornada da consulta do SQLite
             string[] desiredColumns = { "name", "id" };
-            string[] searchableColumns = { "name" };
+            string[] searchableColumns = { "name" }; // Colunas usadas na busca
 
             var columnMappings = new Dictionary<string, string>
             {
                 { "name", "Nome" },
             };
 
+            // A chamada das funções é feita pelo BaseForm, que é a classe mãe desse formulário
             ConfigureSearchDataViewer(dataViewer, searchString, "role_play_games", desiredColumns, columnMappings, searchableColumns, null);
+
+            // Oculta as colunas especificadas
             OccultColumns(dataViewer, "id");
         }
 
+        //Criação dos botões de editar e deletar em cada linha de forma automática
         private void DataViewer_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             if (e.RowIndex < 0) return;
