5ca91b66163effd1bd9ad95ff5e803401cb5b7ca
Pedro-Landgraf <143462985+Pedro-Landgraf@users.noreply.github.com>
2025-06-17 13:09:40 -0300

    Update RPGCampaignDelete.cs

--- projeto-ludico/View/RPGForms/RPGManagerForms/RPGCampaignDelete.cs
+++ projeto-ludico/View/RPGForms/RPGManagerForms/RPGCampaignDelete.cs
@@ -20,7 +20,9 @@ namespace projeto_ludico.View.RPGForms
 
         public void DeleteCampaignRPG(DataGridViewRow row)
         {
+             //Análisa se o id é diferente de nulo e se consegue converter para INT
             if (row.Cells["id"].Value != null && int.TryParse(row.Cells["id"].Value.ToString(), out int id)) {
+                // Exibe uma mensagem de confirmação, se o usuário apertar Sim, irá deletar pelo controller
                 DialogResult result = MessageBox.Show("Tem certeza de que deseja excluir esta campanha?", "Confirmação", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
 
                 if (result == DialogResult.Yes) {
