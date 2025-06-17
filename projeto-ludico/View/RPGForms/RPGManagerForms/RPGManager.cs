c74c90499d2ce615da6cbbf64bb17da081ef7a33
Pedro-Landgraf <143462985+Pedro-Landgraf@users.noreply.github.com>
2025-06-17 13:07:09 -0300

    Update RPGManager.cs

--- projeto-ludico/View/RPGForms/RPGManagerForms/RPGManager.cs
+++ projeto-ludico/View/RPGForms/RPGManagerForms/RPGManager.cs
@@ -25,6 +25,7 @@ namespace projeto_ludico.View.RPGForms
             ConfigureRPGViewer();
         }
 
+        //Realiza a chamada das informações do SQL, passamos as colunas que queremos e as renomeamos no da DataGrid
         private void ConfigureRPGViewer() {
             string[] desiredColumns = { "name", "id" };
             var columnMappings = new Dictionary<string, string>
@@ -49,20 +50,24 @@ namespace projeto_ludico.View.RPGForms
             OccultColumns(dataViewer, "id");
         }
 
-
+         //Função responsável para realizar a atualização de pesquisa quando o usuário realizar a pesquisa no formulário
         private void PerformSearch(string searchString) {
+            // Passamos o nome da coluna que queremos que seja retornada da consulta do SQLite
             string[] desiredColumns = { "name", "id" };
-            string[] searchableColumns = { "name" };
+            string[] searchableColumns = { "name" }; // Colunas usadas na busca
 
             var columnMappings = new Dictionary<string, string>
             {
                 { "name", "Nome da Campanha" },
             };
 
+            // A chamada das funções é feita pelo BaseForm, que é a classe mãe desse formulário
+            //Passamos o null no final pois não temos nenhum JOIN a ser retornado na tabela
             ConfigureSearchDataViewer(dataViewer, searchString, "role_play_games_campaigns", desiredColumns, columnMappings, searchableColumns, null);
-            OccultColumns(dataViewer, "id");
+            OccultColumns(dataViewer, "id"); // Oculta as colunas especificadas
         }
 
+        //Criação dos botões de editar e deletar em cada linha de forma automática
         private void DataViewer_CellContentClick(object sender, DataGridViewCellEventArgs e) {
             if (e.RowIndex < 0) return;
 
