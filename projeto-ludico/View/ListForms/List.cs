4af721069882ace43652d020ffe29a34dd6ac96c
Pedro-Landgraf <143462985+Pedro-Landgraf@users.noreply.github.com>
2025-06-17 13:25:05 -0300

    Update List.cs

--- projeto-ludico/View/ListForms/List.cs
+++ projeto-ludico/View/ListForms/List.cs
@@ -14,7 +14,8 @@ namespace projeto_ludico.View.ListForms
             ConfigureListViewer();
             dataViewer.CellContentClick += DataViewer_CellContentClick;
         }
-
+        
+        //Realiza a chamada das informações do SQL, passamos as colunas que queremos e as renomeamos no da DataGrid
         private void ConfigureListViewer()
         {
             string[] desiredColumns = { "name", "id"};
@@ -28,19 +29,25 @@ namespace projeto_ludico.View.ListForms
             OccultColumns(dataViewer, "id");
         }
 
+         //Função responsável para realizar a atualização de pesquisa quando o usuário realizar a pesquisa no formulário
         private void PerformSearch(string searchString)
         {
+            // Passamos o nome da coluna que queremos que seja retornada da consulta do SQLite
             string[] desiredColumns = { "name", "id"};
-            string[] searchableColumns = { "name" };
+            string[] searchableColumns = { "name" }; // Colunas usadas na busca
 
             var columnMappings = new Dictionary<string, string>
             {
                 { "name", "Nome da Lista" },
             };
 
+            // A chamada das funções é feita pelo BaseForm, que é a classe mãe desse formulário
+            //Passamos o null no final pois não temos nenhum JOIN a ser retornado na tabela
             ConfigureSearchDataViewer(dataViewer, searchString, "lists", desiredColumns, columnMappings, searchableColumns, null);
-            OccultColumns(dataViewer, "id");
+            OccultColumns(dataViewer, "id"); // Oculta as colunas especificadas
         }
+
+        //Criação dos botões de editar e deletar em cada linha de forma automática
         private void DataViewer_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             if (e.RowIndex < 0) 
